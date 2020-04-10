6a5205cf3cc276944c9946a8cb0e10e502b12ffd
Shah Wajahat <shahwajahatijaz@gmail.com>
2020-04-11 03:07:30 +0500

    how we handle mouse event mouse left and right click effect

---
+++ mouseeventonimage.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+
+def click_event(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(x,",",y)
+        font = cv2.FONT_HERSHEY_COMPLEX
+        strXY = str(x) + ',' +str(y)
+        cv2.putText(img,strXY, (x,y),font,1,(255,255,0),2 )
+        cv2.imshow("image",img)
+    if event == cv2.EVENT_RBUTTONDOWN:
+        blue = img[y,x,0]
+        green =img[y,x,1]
+        red =img[y,x,2]
+        font= cv2.FONT_HERSHEY_COMPLEX
+        strBGR = str(blue)+','+str(green)+','+str(red)
+        cv2.putText(img,strBGR,(x,y),font,1,(45,55,255),2)
+        cv2.imshow("image",img)
+
+#img =np.zeros((512,512,3),np.uint8)
+img =cv2.imread('image1.jpg')
+cv2.imshow("image",img)
+
+cv2.setMouseCallback("image",click_event)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
